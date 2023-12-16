9c90288fcbae6238e90132ae7c88f389d5f800ec
Pio <pio.cho.bia@gmail.com>
2023-12-16 07:09:32 +0100

    zakończony temat

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxPokaz = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxWpisz = new System.Windows.Forms.TextBox();
+            this.buttonWloz = new System.Windows.Forms.Button();
+            this.buttonZdejmij = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxPokaz
+            // 
+            this.textBoxPokaz.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBoxPokaz.Location = new System.Drawing.Point(122, 167);
+            this.textBoxPokaz.Multiline = true;
+            this.textBoxPokaz.Name = "textBoxPokaz";
+            this.textBoxPokaz.ReadOnly = true;
+            this.textBoxPokaz.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxPokaz.Size = new System.Drawing.Size(223, 475);
+            this.textBoxPokaz.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(114, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "wkładanie i usuwanie z stosu";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxWpisz
+            // 
+            this.textBoxWpisz.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBoxWpisz.Location = new System.Drawing.Point(122, 61);
+            this.textBoxWpisz.Name = "textBoxWpisz";
+            this.textBoxWpisz.Size = new System.Drawing.Size(223, 29);
+            this.textBoxWpisz.TabIndex = 2;
+            // 
+            // buttonWloz
+            // 
+            this.buttonWloz.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonWloz.Location = new System.Drawing.Point(122, 109);
+            this.buttonWloz.Name = "buttonWloz";
+            this.buttonWloz.Size = new System.Drawing.Size(91, 35);
+            this.buttonWloz.TabIndex = 3;
+            this.buttonWloz.Text = "wloz";
+            this.buttonWloz.UseVisualStyleBackColor = true;
+            this.buttonWloz.Click += new System.EventHandler(this.buttonWloz_Click);
+            // 
+            // buttonZdejmij
+            // 
+            this.buttonZdejmij.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonZdejmij.Location = new System.Drawing.Point(254, 109);
+            this.buttonZdejmij.Name = "buttonZdejmij";
+            this.buttonZdejmij.Size = new System.Drawing.Size(91, 35);
+            this.buttonZdejmij.TabIndex = 4;
+            this.buttonZdejmij.Text = "zdejmij";
+            this.buttonZdejmij.UseVisualStyleBackColor = true;
+            this.buttonZdejmij.Click += new System.EventHandler(this.buttonZdejmij_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(532, 670);
+            this.Controls.Add(this.buttonZdejmij);
+            this.Controls.Add(this.buttonWloz);
+            this.Controls.Add(this.textBoxWpisz);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPokaz);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxPokaz;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxWpisz;
+        private System.Windows.Forms.Button buttonWloz;
+        private System.Windows.Forms.Button buttonZdejmij;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -17,5 +17,50 @@ namespace WinForms_Stos
             InitializeComponent();
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private string[] stos = new string[0];
+        int dl;//dlugosc tabeli
+
+        private void buttonWloz_Click(object sender, EventArgs e)
+        {
+            if (textBoxWpisz.Text != "")
+            {
+                textBoxWpisz.Focus();
+                textBoxWpisz.SelectAll();
+                dl = stos.Length + 1;
+                
+                //stworzenie kopi
+                string[] tab = (string[])stos.Clone();
+                //stworzenie nowego obiektu tablicy stos
+                stos = new string[dl];
+                //przekopiowanie zawartości tab do stos
+                tab.CopyTo(stos, 0);
+                //wpisanie elementu z textBox1
+                stos[stos.Length - 1] = textBoxWpisz.Text;
+                //połaczenie elementów tablicy w string
+                textBoxPokaz.Text = String.Join(" ", stos);
+                textBoxWpisz.Focus();
+            }
+        }
+
+        private void buttonZdejmij_Click(object sender, EventArgs e)
+        {
+            if (stos.Length != 0)
+            {
+                dl = stos.Length - 1;
+                string[] tab = new string[dl];
+                Array.Copy(stos, tab, dl);
+                stos = new string[dl];
+                tab.CopyTo(stos, 0);
+                textBoxPokaz.Text = String.Join(" ", stos);
+            }
+        }
+
+
     }
 }
